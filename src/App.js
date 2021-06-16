9bfbba63cdb7961e27c59b2f7b68899a94f1f930
HIT701-MOUSE\oashi <oaship@gmail.com>
2021-06-16 22:05:12 +0900

    Remove h1 tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello,123 world !!!</h1></div>;
+    return <div>Hello,123 world !!!</div>;
   }
 }
 
