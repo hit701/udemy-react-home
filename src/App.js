b6d8d6064687e2d541905fb34298ce23c29ea99e
hit701 <oaship@gmail.com>
2021-06-17 10:29:00 +0900

    Adding action.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component} from 'react';
 
-// class App extends Component {
-//   render(){
-//     return <h1>Hello, world-----pppp------</h1>;
-//   }
-// }
+const App = () => (<Counter></Counter>)
 
-const App = () => {
-  return (
-  <div>
-    <Cat />
-    <Cat />
-    <Cat />
-    <Cat />
-  </div>
-  )
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    //console.log(this.state)
+    this.state = { count: 0}
+  }
 
-const Cat = () => {
-  return <div>Meow!</div>
+  handlePlusButton = () => {
+    console.log("handlePlusButton")
+    console.log(this.state.count)
+    const count = this.state.count
+    this.setState( { count: this.state.count + 1})
+  }
+  handleMinusButton = () => {
+    console.log("handleMinusButton")
+    console.log(this.state.count)
+    const count = this.state.count
+    this.setState( { count: this.state.count - 1})
+  }
+  render() {
+    return (
+    <React.Fragment>
+       <div>count: { this.state.count }</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>-1</button>
+    </React.Fragment>
+    )
+  }
+  
 }
 export default App;
+
+
 
