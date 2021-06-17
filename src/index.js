ebbe1c043ef4a2c0cdfba1a0eada54df1e10af9f
hit701 <oaship@gmail.com>
2021-06-17 11:23:55 +0900

    Implement store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore( render)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
