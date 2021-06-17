f74098931785079ef255383bba9655e02b19fba3
HIT701-MOUSE\oashi <oaship@gmail.com>
2021-06-17 19:32:17 +0900

    Props Type and error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     {name:"Taro", age:10},
     {name:"Hanako", age:5},
     {name: "Yuto", age:20},
-    {name: "Papa"}
+    {name: 1}
   ]
   return (
     <div>
@@ -22,8 +23,12 @@ const User = (props) => {
   return <div>Hi!, I am {props.name}, and, {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 4
+User.PropType = {
+  name:PropTypes.string,
+  age: PropTypes.number
 }
+//   namer: PropTypes.string
+//   //age: PropTypes.number
+// }
 
 export default App;
