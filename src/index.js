9d137a1d2f838a3929ae7f3d8a3bd9e687048156
HIT701-MOUSE\oashi <oaship@gmail.com>
2021-06-18 18:44:35 +0900

    Store and app components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { createStore} from 'redux'
 import { Provider } from 'react-redux';
 
 import './index.css';
-import { reducer } from 'reducers';
-import App from './App';
+import reducer from './reducers';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
